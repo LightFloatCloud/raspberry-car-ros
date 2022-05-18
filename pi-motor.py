03f844a1739be722b9797b4cf5267599e5bd6efc
LightFloatCloud <92479063+LightFloatCloud@users.noreply.github.com>
2022-05-18 10:18:51 +0800

    Add files via upload

--- pi-motor.py
+++ pi-motor.py
@@ -3,17 +3,32 @@ import sys
 import time
 import RPi.GPIO as GPIO
 
+import rospy
+from geometry_msgs.msg import Twist
+
 # Use BCM GPIO references
 # instead of physical pin numbers
 
-
+WHEEL_WIDTH = 0.123
+WHEEL_LENGTH = 0.11
 # Define GPIO signals to use
 # Physical pins 12,16,18,22 -> GPIO[18,23,24,25]
 # [GPIO18:INT1, GPIO23:INT2, GPIO24:INT3, GPIO25:INT4]
 
 class motor():
     version = '0.01'
-    speed = [0, 0]
+    speed = [0.0, 0.0] # speed值域：0~100
+    PWM_frequency = 10.0
+    
+    drive_twist2vel_scale = 100
+    drive_twist2ang_scale = 1 * WHEEL_WIDTH/2
+    
+    # 初始化 ros 节点
+    rospy.init_node("Twist_Driver", anonymous=True)
+
+    # 初始化控制命令发布者
+    cmd_vel_pub = rospy.Subsciber('cmd_vel', Twist, twist_process_CallBack, queue_size=2)
+    
 
     def __init__(self, LeftPin=[18, 23], RightPin=[24, 25]):
         self.left  = LeftPin
@@ -21,8 +36,8 @@ class motor():
         GPIO.setmode(GPIO.BCM)
         for pin in (self.left + self.right):
             GPIO.setup(pin, GPIO.OUT)
-        self.left_pwm  = [GPIO.PWM(self.left[0],  100), GPIO.PWM(self.left[1],  100)]
-        self.right_pwm = [GPIO.PWM(self.right[0], 100), GPIO.PWM(self.right[1], 100)]
+        self.left_pwm  = [GPIO.PWM(self.left[0],  self.PWM_frequency), GPIO.PWM(self.left[1],  self.PWM_frequency)]
+        self.right_pwm = [GPIO.PWM(self.right[0], self.PWM_frequency), GPIO.PWM(self.right[1], self.PWM_frequency)]
 
     def start(self):
         left_speed = self.speed[0]
@@ -48,12 +63,12 @@ class motor():
  
 
     # speed: 0 ~ 100 
-    def run_speed(self, left_speed, right_speed):
+    def run_speed(self, left_speed, right_speed): # speed值域：0~100
         self.speed[0] = left_speed 
         self.speed[1] = right_speed
     
         if left_speed > 0 :
-            self.left_pwm[0].ChangeDutyCycle(left_speed)
+            self.left_pwm[0].ChangeDutyCycle(left_speed) 
             self.left_pwm[1].ChangeDutyCycle(0)
         else:
             self.left_pwm[0].ChangeDutyCycle(0)
@@ -64,10 +79,38 @@ class motor():
         else:
             self.right_pwm[0].ChangeDutyCycle(0)
             self.right_pwm[1].ChangeDutyCycle(-right_speed)
+    def twist_process_CallBack(msg):
+        rospy.loginfo("Base vel: x:%06f, y:%0.6f, ang: z:%0.6f",\
+            msg.linear.x, msg.linear.y, msg.angular.z)
+        
+        self.speed[0] = (msg.linear.x - drive_twist2ang_scale*msg.angular.z) * drive_twist2vel_scale
+        self.speed[1] = (msg.linear.x + drive_twist2ang_scale*msg.angular.z) * drive_twist2vel_scale
+        if self.speed[0]>100 :
+            self.speed[0] = 100
+        elif self.speed[0]<-100 :
+            self.speed[0] = -100
+        
+        if self.speed[1]>100 :
+            self.speed[1] = 100
+        elif self.speed[1]<-100 :
+            self.speed[1] = -100
+            
+        self.start()
 
-if __function__ ==  __main__:
+
+            
     
+
+def main():
+    TestState = True
     m = motor()
-    cmd_speed = int(input("Set speed:"))
-    m.run_speed(cmd_speed, cmd_speed)
-    m.start()
+    
+    while TestState:
+        cmd_speed = int(input("Set speed:"))
+        m.run_speed(cmd_speed, cmd_speed)
+        m.start()
+        
+    rospy.spin()
+
+if __name__ ==  '__main__':
+    main()
--- speed_recorder.py
+++ speed_recorder.py
@@ -1,14 +1,23 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 # Import required libraries
 import sys
 import time
 import RPi.GPIO as GPIO
-
+import rospy
+import math
 # Use BCM GPIO references
 # instead of physical pin numbers
+from nav_msgs.msg  import Odometry
 
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 
+#import roslib
+#roslib.load_manifest('learning_tf')
 
+import tf
+#import turtlesim.msg
 
 
 
@@ -16,13 +25,34 @@ import RPi.GPIO as GPIO
 # Physical pins 12,16,18,22 -> GPIO[18,23,24,25]
 # [GPIO18:INT1, GPIO23:INT2, GPIO24:INT3, GPIO25:INT4]
 
+# GPIO.BCM LeftPin=[27], RightPin=[22]
 
+WHEEL_WIDTH = 0.123
+WHEEL_LENGTH = 0.11
 
 class speed_count():
+    # Param Set
+    pos = [0.0, 0.0, 0.0, 0.0] #x, y, z, theta
+    speed = [0.0, 0.0]
+    sample_time_freq = 10.0
+    speed_scale = 1
+    
+    # Initialization
+    rospy.init_node('velocity_publisher', anonymous=True)
+    
+    virtual_speed = rospy.get_param('virtual_speed', False)
+    virtual_speed_value = rospy.get_param('virtual_speed_value', [0.1, 0.105])
+    
+    rate = rospy.Rate(sample_time_freq)
+    odom_pos_pub = rospy.Publisher('/odom', Odometry, queue_size=10)
+    
+    
     left_edge_sum = 0
     right_edge_sum = 0
-    speed = [0, 0]
-    sample_time_inteval = 0.2
+    sample_time_inteval = 1.0/sample_time_freq
+    
+    
+    
 
     def __init__(self, LeftPin=[27], RightPin=[22]):
         self.left  = LeftPin
@@ -32,41 +62,106 @@ class speed_count():
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
             GPIO.add_event_detect(pin, GPIO.RISING, callback=self.my_callback )#在通道上添加上升临界值检测
         
-        while True:
+        while not rospy.is_shutdown():
             #GPIO.wait_for_edge(12, GPIO.RISING)
             # GPIO.RISING    上升沿检测
             # GPIO.FALLING   下降沿检测
             # GPIO.BOTH      两者都可以，也就是说检测到边沿变化
             
-            time.sleep(sample_time_inteval)
-            speed = [left_edge_sum/sample_time_inteval, right_edge_sum/sample_time_inteval]
-            left_edge_sum = 0
-            right_edge_sum = 0 
-            print("Recent speed:%s" %speed)
-            
+            #time.sleep(self.sample_time_inteval)
+            if(self.virtual_speed == False):
+                self.speed = [self.left_edge_sum/self.sample_time_inteval, self.right_edge_sum/self.sample_time_inteval]
+                self.left_edge_sum = 0
+                self.right_edge_sum = 0 
+                
+                self.speed[0] = self.speed[0]*self.speed_scale
+                self.speed[1] = self.speed[1]*self.speed_scale
+                
+            else:
+                self.speed = self.virtual_speed_value
+
+            print("Recent speed:%s" %self.speed)
+            self.pos_change()
+            self.publishOdometry()
+            self.rate.sleep()
   
 
     def my_callback(self, channel):
-        if(channel == self.left)
-            left_edge_sum = left_edge_sum + 1
-        elif(channel == self.right)
-            right_edge_sum = right_edge_sum + 1
+        if(channel == self.left[0]):
+            self.left_edge_sum = self.left_edge_sum + 1
+        elif(channel == self.right[0]):
+            self.right_edge_sum = self.right_edge_sum + 1
         
         
         print("在通道%s上进行边缘检测" %channel)
 
     def get_inteval(self):
-        return sample_time_inteval
+        return self.sample_time_inteval
     
     def get_speed(self):
-        return speed
+        return self.speed
         
-if __function__ ==  __main__:
-    
-    speedcount_example = speed()
-    
-    
-    
-    
-    
         
+    def vel_move(self):
+        return (self.speed[0]+self.speed[1])/2 
+        
+    def vel_turn(self):
+        return (self.speed[1]-self.speed[0])/WHEEL_WIDTH
+        
+    def pos_change(self):
+        v = self.vel_move()
+        w = self.vel_turn()
+        R = v / w * self.sample_time_inteval
+        
+        delta_theta = w * self.sample_time_inteval
+        
+        L = 2 * R * math.sin(delta_theta/2)
+        
+        delta_x = L * math.cos(self.pos[3] + v * self.sample_time_inteval/2)
+        delta_y = L * math.sin(self.pos[3] + w * self.sample_time_inteval/2)
+        
+        self.pos[0] = self.pos[0] + delta_x
+        self.pos[1] = self.pos[1] + delta_y
+        self.pos[2] = self.pos[2] + 0
+        self.pos[3] = self.pos[3] + delta_theta
+        
+    def publishOdometry(self):
+        #def handle_pose(msg, carname):
+        odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.pos[3])
+        current_time = rospy.Time.now()
+        br = tf.TransformBroadcaster()
+        br.sendTransform((self.pos[0], self.pos[1], 0),
+                     odom_quat,
+                     current_time,
+                     "base_link",
+                     "odom")
+        
+        # next, we'll publish the odometry message over ROS
+        odom = Odometry()
+        odom.header.stamp = current_time
+        odom.header.frame_id = "odom"
+
+        # set the position
+        odom.pose.pose = Pose(Point(self.pos[0], self.pos[1], 0.), Quaternion(*odom_quat))
+        
+        odom.pose.covariance[0] = 0.1;
+        odom.pose.covariance[7] = 0.1;
+        odom.pose.covariance[14] = 0.1;
+        odom.pose.covariance[21] = 1.0;
+        odom.pose.covariance[28] = 1.0;
+        odom.pose.covariance[35] = 1.0;
+
+        # set the velocity
+        odom.child_frame_id = "base_link"
+        #odom.twist.twist = Twist(Vector3(self.vel_move(), 0, 0), Vector3(0, 0, self.vel_turn()))
+
+        # publish the message
+        self.odom_pos_pub.publish(odom)
+
+        
+
+if __name__ ==  '__main__':
+    
+    speedcount_example = speed_count()
+    
+    
